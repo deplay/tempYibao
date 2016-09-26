7539b3094664e90bb5c757a7fb9707bbcc17d3c6
deplay <1150696014@qq.com>
2016-09-26 13:37:00 +0800

    lilei

--- www/yibao/develop/js/service/product/productListService.js
+++ www/yibao/develop/js/service/product/productListService.js
@@ -1,40 +1,6 @@
 define(['yibao'], function(yibao) {
     yibao.service('$productListService', ['$q', '$data', '$http', '$getUrl', function($q, $data, $http, $getUrl) {
         this.loadData = function(id, again, pageIndex, sort) {
-            $data.set('ns1', 'hello', 'world');
-            $data.get('ns1', 'hello').then(function(res) {
-                console.log(res);
-            })
-            $data.get('ajax', {
-                url: $getUrl('productList') + id,
-                method: 'GET',
-                params: {
-                    q: again + ':relevance:productType:TUANGOU:productType:YXDJ',
-                    page: pageIndex,
-                    sort: sort,
-                    channe: 'hxyxt'
-                }
-            }, true).then(function(res) {
-                console.log(res);
-
-
-                // 第二次获取
-                $data.get('ajax', {
-                    url: $getUrl('productList') + id,
-                    method: 'GET',
-                    params: {
-                        q: again + ':relevance:productType:TUANGOU:productType:YXDJ',
-                        page: pageIndex,
-                        sort: sort,
-                        channe: 'hxyxt'
-                    }
-                }, true).then(function(res) {
-                    console.log(res);
-                });
-            });
-
-
-
             var deferred = $q.defer();
             var url = $getUrl('productList') + id;
             var paras = {
@@ -42,13 +8,13 @@ define(['yibao'], function(yibao) {
                 page: pageIndex,
                 sort: sort,
                 channe: 'hxyxt'
-            }
-            $http({
+            };
+            $data.get('ajax', {
                 url: url,
                 method: 'GET',
                 params: paras
-            }).then(function(res) {
-                deferred.resolve(res);
+            }, true).then(function(res) {
+                deferred.resolve(res.data);
             }, function(err) {
                 deferred.reject(err);
             });
